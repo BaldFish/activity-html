d2178f4bb16b0dd3f26e3df5a406a8edfc616aad
lwc <wence.lv@cnlaunch.com>
2019-05-25 15:21:11 +0800

    goloclass,golomaster

---
+++ src/components/goloclass/technician1.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="technician1">
+    <img src="../../common/images/03.png" alt="yure">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "technician1",
+    components: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    beforeMount() {
+    },
+    mounted() {
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+  }
+</script>
+
+<style scoped lang="stylus">
+  .technician1 {
+    font-size 0
+    width 750px
+    height 1334px
+    img{
+      width 100%
+      height 100%
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import myTechnician1 from '@/components/golocars/technician1'
+import myGolocarsTechnician1 from '@/components/golocars/technician1'
+import myGoloclassTechnician1 from '@/components/goloclass/technician1'
+import myGolomasterTechnician1 from '@/components/golomaster/technician1'
 Vue.use(Router);
 
 export default new Router({
@@ -13,7 +15,17 @@ export default new Router({
     {
       path:'/golocars/technician/1',
       name:'technician1',
-      component:myTechnician1
+      component:myGolocarsTechnician1
+    },
+    {
+      path:'/goloclass/technician/1',
+      name:'technician1',
+      component:myGoloclassTechnician1
+    },
+    {
+      path:'/golomaster/technician/1',
+      name:'technician1',
+      component:myGolomasterTechnician1
     },
   ]
 })
